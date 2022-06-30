94294a723edad43292e2d7e362b460f367b672f8
johng1990 <johngraham.0990@googlemail.com>
2022-06-30 12:07:23 +0100

    Initial PlayerClass Commit

---
+++ Monopoly/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    class Player
+    {
+        public int Money { get; set; } = 1500;
+        public bool InJail { get; set; } = false;
+        //public Space* CurrentSpace { get; set; }
+        //public List<Space*> OwnedProperties { get; set; }
+
+        public Player()
+        {
+        }
+
+        public void TakeTurn()
+        {
+            //roll dice
+            //move position
+            //
+        }
+        /*
+	* Paying out
+	* Receiving money
+	* Rolling dice
+	* Buying space/house/hotel
+	* Move
+	* Collect when passing go
+         */
+    }
+}
